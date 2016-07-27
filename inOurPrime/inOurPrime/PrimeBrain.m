72f673ad11fd2439137368809a2d88791eb4dd07
Jorge Catalan Jr <catalanjrj@gmail.com>
2016-07-27 16:38:44 -0400

    PrimeBrain does math

--- inOurPrime/inOurPrime/PrimeBrain.m
+++ inOurPrime/inOurPrime/PrimeBrain.m
@@ -22,6 +22,41 @@
     return YES;
     
 }
+-(NSArray*)RetreiveFactorsForNumber:(NSUInteger)number{
+    int counter = 2;
+    int range = sqrt((int)number);
+    NSMutableArray *Factors = nil;
+    
+    while(counter <= range){
+        if((number % counter == 0) && [self isPrimeNumber:counter]){
+            if(!Factors){
+                Factors = [[NSMutableArray alloc]init];
+            }
+            [Factors addObject:[NSString stringWithFormat:@"%d",counter]];
+        }
+        counter += 1;
+    
+    }
+    return Factors;
+
+}
+
+-(NSInteger)getGreatestCommonFactor:(NSUInteger)firstNumber numberTwo:(NSUInteger)secondNumber{
+    NSArray *firstNumberArray = [[NSArray alloc]initWithArray:[self RetreiveFactorsForNumber:firstNumber ]];
+    NSArray *secondNumberArray = [[NSArray alloc]initWithArray:[self RetreiveFactorsForNumber:secondNumber]];
+    NSInteger greatesCommonFactor = 0;
+    
+    if(firstNumberArray && secondNumberArray){
+        for(NSString *first in firstNumberArray){
+            for(NSString *second in secondNumberArray){
+                if(([first integerValue] == [second integerValue]) &&([first integerValue] > greatesCommonFactor)) {
+                    greatesCommonFactor = [first integerValue];
+                }
+            }
+        }
+    }
+    return greatesCommonFactor;
+}
 
 
 @end
