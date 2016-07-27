b5d79641a2abb9aebf4124054bc7e2e8377a59c5
Jorge Catalan Jr <catalanjrj@gmail.com>
2016-07-27 18:08:49 -0400

    making all connections to storyboard

--- inOurPrime/inOurPrime/ViewController.m
+++ inOurPrime/inOurPrime/ViewController.m
@@ -7,8 +7,24 @@
 //
 
 #import "ViewController.h"
+#import "PrimeBrain.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *instructionsLabel;
+@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+@property (weak, nonatomic) IBOutlet UIButton *doMathButton;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+@property (weak, nonatomic) IBOutlet UIButton *primeNumberButton;
+@property (weak, nonatomic) IBOutlet UIButton *factorsButton;
+@property (weak, nonatomic) IBOutlet UIButton *GreatestCommonFactorButton;
+
+@property(strong,nonatomic)PrimeBrain *brain;
+
+-(NSString *)formatString:(NSArray *)numbersArray;
+-(void)checkForPrimeNumber:(NSString *)numbersString;
+-(void)checkFactors:(NSString *)numbersString;
+-(void)checkGreatesCommonFactor:(NSString *)numbersString;
+-(void)displayError:(NSString *)title message:(NSString *)message;
 
 @end
 
@@ -16,12 +32,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //initialize PrimeBrain
+    self.brain = [[PrimeBrain alloc]init];
+}
+//create action that does math for selection
+- (IBAction)doTheMath:(id)sender {
+    if ([sender isEqual:self.primeNumberButton]) {
+        [self checkForPrimeNumber:self.numberTextField.text];
+        
+    }else if ([sender isEqual:self.factorsButton]){
+        [self checkFactors:self.numberTextField.text];
+    
+    }else if ([self.numberTextField.text componentsSeparatedByString:@" "].count >=2){
+        [self checkGreatesCommonFactor:self.numberTextField.text];
+        
+    }else{
+        [self displayError:@"Error!!" message:@"Enter two numbers seperated by a space"];
+    
+    }
+    [self.numberTextField resignFirstResponder];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
